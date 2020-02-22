62dc60f2a55e2e552a7213816126162e41e90262
Josh Jacobs <josh@joshjacobs.net>
2020-02-22 11:23:46 +1000

    Read PORT From Env Vars

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
